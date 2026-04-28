6ae18a3efb48b21e7eff0118a91896b69273da33
Piotrusbnz <pherebnz@gmail.com>
2026-04-28 15:59:37 +0200

    parser.py

--- parser.py
+++ parser.py
@@ -1,63 +1,6 @@
-import ply.lex as lex
 import ply.yacc as yacc
-import sys
+from lexer import tokens
 
-#SKANER
-tokens = (
-    'HAI', 'KTHXBYE', 'NEWLINE', 
-    'VAR_DEC', 'ITZ', 'VISIBLE', 
-    'NUMBR', 'YARN', 'ID',
-    'SUM', 'AN'
-)
-
-t_HAI = r'HAI'
-t_KTHXBYE = r'KTHXBYE'
-t_ITZ = r'ITZ'
-t_VISIBLE = r'VISIBLE'
-t_AN = r'AN'
-
-def t_VAR_DEC(t):
-    r'I\s+HAS\s+A'
-    return t
-
-def t_SUM(t):
-    r'SUM\s+OF'
-    return t
-
-def t_ID(t):
-    r'[a-zA-Z][a-zA-Z0-9_]*'
-    if t.value in ['HAI', 'KTHXBYE', 'ITZ', 'VISIBLE', 'AN']:
-        t.type = t.value
-    return t
-
-def t_NUMBR(t):
-    r'[0-9]+'
-    t.value = int(t.value)
-    return t
-
-def t_YARN(t):
-    r'"[^"]*"'
-    return t
-
-def t_NEWLINE(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-    return t
-  
-def t_COMMENT(t):
-    r'BTW.*'
-    pass
-
-t_ignore = ' \t'
-
-def t_error(t):
-    print(f"Nieznany znak: '{t.value[0]}' w linii {t.lexer.lineno}")
-    t.lexer.skip(1)
-
-lexer = lex.lex()
-
-
-#PARSER
 def p_program(p):
     """program : HAI separator statements KTHXBYE
                | HAI separator KTHXBYE"""
@@ -124,20 +67,3 @@ def p_error(p):
         print("Błąd składniowy: Niespodziewany koniec pliku")
 
 parser = yacc.yacc()
-
-
-#URUCHOMIENIE
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Użycie: python transpiler.py wejscie.lol wyjscie.py")
-        sys.exit(1)
-
-    with open(sys.argv[1], 'r', encoding='utf-8') as f:
-        kod_lol = f.read()
-
-    wynik = parser.parse(kod_lol, lexer=lexer)
-
-    if wynik:
-        with open(sys.argv[2], 'w', encoding='utf-8') as f:
-            f.write(wynik)
-        print(f"Wygenerowano plik: {sys.argv[2]}")
