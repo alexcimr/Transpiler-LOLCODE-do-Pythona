93076a54cc75f87d728b79ed56aefa1b829266ef
Piotrusbnz <pherebnz@gmail.com>
2026-04-28 16:16:56 +0200

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import sys
+from lexer import lexer
+from parser import parser
 
-if __name__ == '__main__':
+def main():
     if len(sys.argv) < 3:
-        print("Użycie: python transpiler.py wejscie.lol wyjscie.py")
+        print("Użycie: python main.py <plik_wejsciowy.lol> <plik_wyjsciowy.py>")
         sys.exit(1)
 
-    with open(sys.argv[1], 'r', encoding='utf-8') as f:
-        kod_lol = f.read()
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-    wynik = parser.parse(kod_lol, lexer=lexer)
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lol_code = f.read()
 
-    if wynik:
-        with open(sys.argv[2], 'w', encoding='utf-8') as f:
-            f.write(wynik)
-        print(f"Wygenerowano plik: {sys.argv[2]}")
+    lexer.lineno = 1
+    python_result = parser.parse(lol_code, lexer=lexer)
+
+    if python_result:
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(python_result)
+        print(f"Wynik zapisano w: {output_file}")
+    else:
+        print("Translacja przerwana z powodu błędów składniowych.")
+
+if __name__ == '__main__':
+    main()
